a14c377615c16e2b95c7d0871e38abf09df5539b
Suven-p <pandeysuven@gmail.com>
2022-12-28 08:11:35 +0545

    Add canonical url plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,6 +11,13 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://suven.com.np`,
+        stripQueryString: true,
+      },
+    },
     "gatsby-plugin-emotion",
     "gatsby-plugin-image",
     'gatsby-plugin-postcss',
