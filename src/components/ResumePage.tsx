03dcf910195890b8801c99abbd1e127881bd6f19
Suven-p <pandeysuven@gmail.com>
2024-07-14 14:05:47 +0545

    Bug: Zoom toolbar switches color with theme

--- src/components/ResumePage.tsx
+++ src/components/ResumePage.tsx
@@ -33,7 +33,7 @@ const ZoomToolBar: FC<ZoomToolBarProps> = ({ scale, setScale }) => {
         setScale((prev) => Math.max(prev - 10, minValue));
     }
     return (
-        <div className='bg-slate-800 flex justify-center items-center gap-4 fixed top-[6rem] right-[3rem] border rounded-3xl py-3 px-6 z-50'>
+        <div className='bg-slate-200 dark:bg-slate-800 flex justify-center items-center gap-4 fixed top-[6rem] right-[3rem] border rounded-3xl py-3 px-6 z-50'>
             <button
                 className='flex justify-center items-center dark:text-white text-3xl w-6 h-6 rounded-full disabled:opacity-50'
                 onClick={handleZoomIn}
@@ -51,7 +51,8 @@ const ZoomToolBar: FC<ZoomToolBarProps> = ({ scale, setScale }) => {
                 rel='noreferrer'
                 className='text-black dark:text-slate-300 visited:text-black'
             >
-                <img src='/icons/openExternal.svg' alt='Open' className='w-4 h-4' />
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className='w-4 h-4 fill-black dark:fill-white'><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
+
             </a>
         </div>
     );
